ed0aceec0883da70c00a92804ea7d924b05bfa60
AdamLeibovitz <adamrleibovitz@gmail.com>
2024-11-10 21:32:30 +0000

    Homework Week 1 Fourth Draft

--- Bill3.java
+++ Bill3.java
@@ -7,6 +7,6 @@ public class Bill3 {
 		String name3 = args[2];
 	    int bill = Integer.parseInt(args[3]);
 		double num = bill;
-		System.out.println("Dear "+name1+", "+name2+", and "+name3+": pay "+Math.ceil(num/3)+" Shekels each");
+		System.out.println("Dear "+name3+", "+name2+", and "+name1+": pay "+Math.ceil(num/3)+" Shekels each");
 	}
 }
